1961ecfd0584ff58641281a7433a98fbaeb4d5fe
Daniel Griffen <daniel@griffen.io>
2019-05-24 12:44:54 -0700

    switch to sdk-style projects

--- src/TerminalServiceTests/Properties/AssemblyInfo.cs
+++ src/TerminalServiceTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TerminalServiceTests")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TerminalServiceTests")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
